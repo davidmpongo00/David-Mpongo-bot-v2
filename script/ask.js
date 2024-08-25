4f0897677d4425be23294fa54d8345863a82c422
Mpongo1david <sarutomadara4@gmail.com>
2024-08-25 15:56:40 +0100

    Update ask.js

--- script/ask.js
+++ script/ask.js
@@ -22,7 +22,7 @@ module.exports.run = async function ({ api, event, args }) {
   try {
     const prompt = args.join(" ");
     if (!prompt) {
-      await api.sendMessage("𝗣𝗛𝗔𝗥𝗢𝗨𝗞 ✦\n━━━━━━━━━━━\n Hey master how can help you ?", event.threadID);
+      await api.sendMessage("𝔻𝕒𝕧𝕚𝕕✰ ✦\n━━━━━━━━━━━\n Hey master how can help you ?", event.threadID);
       return;
     }
     const response = await axios.get(`https://sandipbaruwal.onrender.com/gpt?prompt=${encodeURIComponent(prompt)}`);
@@ -37,7 +37,7 @@ module.exports.run = async function ({ api, event, args }) {
       }
     }
 
-    await api.sendMessage(`𝗣𝗛𝗔𝗥𝗢𝗨𝗞 ✦ \n━━━━━━━━━━━\n${formattedAnswer} ☕`, event.threadID);
+    await api.sendMessage(`𝔻𝕒𝕧𝕚𝕕✰ ✦ \n━━━━━━━━━━━\n${formattedAnswer} ☕`, event.threadID);
   } catch (error) {
     console.error("Error:", error.message);
   }
